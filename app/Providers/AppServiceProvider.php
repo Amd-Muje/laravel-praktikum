4a94c2db4e0829696774a87379d4e7fe9c7a9b46
Ahmad Mujahid (Muje) <149925364+Amd-Muje@users.noreply.github.com>
2025-05-31 13:27:53 +0800

    new 3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if(env('APP_ENV') === 'production'){
+            URL::forceScheme('https');
+        }
         Gate::define("view-srudent", function (User $user){
             if ($user->role === "admin" || $user->role === "guest"){
                 return true;
